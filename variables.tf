e4f9eb5bf35d0abea8e4b9f715cdc6e82f838491
motly1 <114095414+motly1@users.noreply.github.com>
2022-09-28 00:06:57 +0100

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,3 +1,26 @@
 # ------------------------------------------
 # Write your Terraform variable inputs here
 # ------------------------------------------
+
+variable "ip address" {
+  description = "ip address"
+  type        = string
+  default     = []
+}
+
+variable "ttl" {
+  description = "ttl"
+  type        = number
+  default     = 100
+}
+
+variable "zone" {
+  description = "zone"
+  type        = string
+  default     = "example.com"
+}
+  
+  variable "record type" {
+  description = "record type"
+  type        = string
+  default     = "a"
